6f4dd27168f9107f3056dcd085f75562a6635da9
caydende <96990579+caydende@users.noreply.github.com>
2025-03-12 06:04:06 +0200

    routing fix 2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: 'products/:id',  // Handle dynamic product routes using SSR
+    renderMode: RenderMode.Server
+  },
+  {
+    path: '**',  // Keep all other routes prerendered
     renderMode: RenderMode.Prerender
   }
 ];
